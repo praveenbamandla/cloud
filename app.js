f2e058cd679ae24c0fcc84b1350155d09b639aa6
Praveen <praveen.bamandla@gmail.com>
2015-01-06 23:52:49 +0530

    added video streaming support

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ mongo.then(function(db){
 	var tasks = require('./todo.js')(db);
 	
 	var express = require('express');
+	var fs = require('fs');
 
 	var app = express();
 	
@@ -44,6 +45,34 @@ mongo.then(function(db){
 			//console.info(req.session.user);
 			res.json(tasks);
 		});
+	});
+	
+	app.get('/movie',isLogged,function(req,res){
+		var file = "movie.mp4";
+		var range = req.headers.range;
+		var positions = range.replace(/bytes=/, "").split("-");
+		var start = parseInt(positions[0], 10);
+		
+		fs.stat(file, function(err, stats) {
+		  var total = stats.size;
+		  var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+		  var chunksize = (end - start) + 1;
+
+		  res.writeHead(206, {
+			"Content-Range": "bytes " + start + "-" + end + "/" + total,
+			"Accept-Ranges": "bytes",
+			"Content-Length": chunksize,
+			"Content-Type": "video/mp4"
+		  });
+
+		  var stream = fs.createReadStream(file, { start: start, end: end })
+			.on("open", function() {
+			  stream.pipe(res);
+			}).on("error", function(err) {
+			  res.end(err);
+			});
+		});
+		
 	});
 	
 	app.post('/login', function (req, res) {
